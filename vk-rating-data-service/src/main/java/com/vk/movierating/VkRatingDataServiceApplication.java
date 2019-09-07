026861281baf21aa443767154be5af0ae2723998
thanhkieuus <voltakieu@yahoo.com>
2019-09-07 18:30:56 -0500

    First Commit

---
+++ vk-rating-data-service/src/main/java/com/vk/movierating/VkRatingDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vk.movierating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkRatingDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VkRatingDataServiceApplication.class, args);
+	}
+
+}
