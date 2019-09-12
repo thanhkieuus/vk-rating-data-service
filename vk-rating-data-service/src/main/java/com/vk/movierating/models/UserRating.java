f4c6820a8efcdb612360cdfd9f913f3c0e9e66c1
thanhkieuus <voltakieu@yahoo.com>
2019-09-12 11:23:04 -0500

    First Commit

---
+++ vk-rating-data-service/src/main/java/com/vk/movierating/models/UserRating.java
@@ -0,0 +1,29 @@
+package com.vk.movierating.models;
+
+import java.util.List;
+
+import com.vk.movierating.models.Rating;
+
+public class UserRating {
+	
+	private List<Rating> ratings;
+
+	public UserRating() {
+	}
+
+	public UserRating(List<Rating> ratings) {
+		this.ratings = ratings;
+	}
+
+
+	public List<Rating> getRatings() {
+		return ratings;
+	}
+
+	public void setRatings(List<Rating> ratings) {
+		this.ratings = ratings;
+	}
+	
+	
+
+}
